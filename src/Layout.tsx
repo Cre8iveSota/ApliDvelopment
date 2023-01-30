837659fec3a977a24d37a531e66a4be96505555c
Cre8iveSota <cre8ivesot@gmail.com>
2023-01-30 15:12:51 +0900

    fix console error

---
+++ src/Layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./components/organisms/Footer";
+import Header from "./components/organisms/Header";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <div>Hello React San</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
